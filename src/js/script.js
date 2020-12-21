0c6aec59cba4a2311e78b5070561e041e86207e3
ElTru <elzbieta.trusiewicz@gmail.com>
2020-12-21 14:00:11 +0100

    Add favorite book dbclick

--- src/js/script.js
+++ src/js/script.js
@@ -7,6 +7,12 @@
     },
     containerOf: {
       booksList: '.books-list'
+    },
+    class: {
+      favoriteBook: 'favorite',
+    },
+    book: {
+      bookImage: ''
     }
   };
 
@@ -16,6 +22,7 @@
 
   const booksList = document.querySelector(select.containerOf.booksList);
   const allBooks = [];
+  const favoriteBooks = [];
 
   function render() {
     for(let book of dataSource.books) {
@@ -24,7 +31,29 @@
       booksList.appendChild(element);
       allBooks.push(element);
     }
-  };
-  render();
+  }
 
+  function initActions() {
+    for (let book of allBooks) {
+      const bookCover = book.querySelector('.book__image');
+      console.log(bookCover);
+
+      bookCover.addEventListener('dblclick', function(event){
+        event.preventDefault();
+        const id = book.getAttribute('.data-id');
+
+        if(!bookCover.classList.contains(select.class.favoriteBook)){
+          favoriteBooks.push(id);
+          bookCover.classList.add(select.class.favoriteBook);
+        } else { 
+          favoriteBooks.splice(favoriteBooks.indexOf(id));
+          bookCover.classList.remove(select.class.favoriteBook);
+        }
+
+      });
+    }
+  }
+
+  render();
+  initActions();
 }
